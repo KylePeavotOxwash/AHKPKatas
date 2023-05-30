0f2bcecc0ccb2ebd5025bebd864f0078a8236948
Andrew Hutton <andrew@oxwash.com>
2023-05-30 16:03:21 +0100

    Kata-1

--- src/StringCalculator.ts
+++ src/StringCalculator.ts
@@ -1,7 +1,12 @@
 export class StringCalculator{
 
     add(numbers: string): number {
-        throw new Error('not implemented')
+
+        const numbersArray = numbers.split(',')
+
+        const sum = numbersArray.length > 1 ? +numbersArray[0] + +numbersArray[1] : +numbersArray[0]
+
+        return numbers ? sum : 0
     }
 
 }
